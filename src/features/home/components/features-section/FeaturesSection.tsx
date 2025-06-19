5c58c4d199a05293aa03e5c1c29e802d43f9a5ea
ega-erinovian <erinovian69@gmail.com>
2025-06-19 10:14:17 +0700

    feat: fix features styling

--- src/features/home/components/features-section/FeaturesSection.tsx
+++ src/features/home/components/features-section/FeaturesSection.tsx
@@ -3,9 +3,9 @@ import FeaturesItem from "./components/FeaturesItem";
 
 const FeaturesSection = () => {
   return (
-    <section className="py-8 xl:py-12 border-t border-t-gray-300 border-b border-b-gray-300 mb-6">
+    <section className="mb-16">
       <div className="container mx-auto px-4 md:px-0 text-center text-gray-600">
-        <ul className="grid xl:gap-x-48 md:gap-x-10 gap-y-20 md:grid-cols-3">
+        <ul className="grid gap-y-20 md:grid-cols-3">
           {features.map((item, idx) => (
             <FeaturesItem key={idx} {...item} />
           ))}
